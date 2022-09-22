5f371c4d752bb9814e14058f3977410d6195ee41
Julia Skillin <jes323@lehigh.edu>
2022-09-22 16:14:25 -0400

    Staged changes made by Troy

--- 02_TAA_Supplement.R
+++ 02_TAA_Supplement.R
@@ -16,22 +16,22 @@ cat("\014")
 library(tidyverse)
 
 # Storing the path of the current working directory
-Temp <- getwd()
+# Temp <- getwd()
 
-# Switching the working directory to the covid-19-data subfolder
-setwd("./covid-19-data/")
-
-# Reading the us.counties.csv in as a data frame
-COUNTIES <- read_csv("us-counties.csv")
-
-# Switching the working directory back to the project folder
-setwd(Temp)
+# # Switching the working directory to the covid-19-data subfolder
+# setwd("./covid-19-data/")
+# 
+# # Reading the us.counties.csv in as a data frame
+# COUNTIES <- read_csv("us-counties.csv")
+# 
+# # Switching the working directory back to the project folder
+# setwd(Temp)
 
 # Alternative way to access subfolders
 library(here)
 COUNTIES <- read_csv(here("covid-19-data","us-counties.csv"))
 
-# Examining the data
+-# Examining the data
 View(COUNTIES)
 
 # Using filter()to get just Snohomish county in Washington
